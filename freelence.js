2bc1bf5db6d50214092934844964e2026155adbf
unknown <salah-eddine-555>
2025-11-04 10:55:03 +0100

    planifier les fonctions  le fichier freelence.js

---
+++ freelence.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+// // function 1 pour lister les freelencer(son photo, specialisation , leur moyenne)charge depuis json
+// function (){
+
+
+// }
+
+
+
+// // function 2 lorsuqe le clicl il faut afficher les details de freelencer (bio, compétences, projets, tarifs, avis).
+// function (){
+
+
+// }
+
+
+
+
+// // function 3  ( je veux pouvoir modifier mon profil via un formulaire avec validation)
+// function (){
+
+
+// }
+
+
+// // function 4 (fonction pour  filtrer les freelances par spécialité (Développeur Web, Designer, Rédacteur, etc.).)
+// function (){
+
+
+// }
