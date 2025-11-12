8b51df4d84cafeda4d139b8cf293bd7f4257a407
unknown <salah-eddine-555>
2025-11-12 09:37:18 +0100

    modifier la fonction pour filtre des mission par categorie

--- commponents/mission/mission.js
+++ commponents/mission/mission.js
@@ -91,6 +91,10 @@ async function filtrageMission(){
 
   let titreNonRdouble = [...new Set(datamission.map(element => element.title))];
 
+  // console.log(titreNonRdouble);
+ 
+titreNonRdouble.map(elt =>console.log(elt.title))
+
   sectionFiltrage.innerHTML = `
   <button class="btn btn-success col-2 ml-1" id="filtrer">Filtrer</button>
     <select class="col-8" id="selection-value">
@@ -106,43 +110,39 @@ async function filtrageMission(){
   
 
   btnFiltere.addEventListener("click", function() {
-    console.log(valueSelectionner.value);
-    const valeurChoisie = valueSelectionner.value;
-    
-    if(valeurChoisie == "Par Technologie"){
-      alert("veuillez selectionr une technologie a rechercher ");
-      return;
-    }
+                const valeurChoisie = valueSelectionner.value;
 
-    const missionFilters = datamission.filter(mission => mission.title === valeurChoisie)
-
-
-      sectionMission.innerHTML = missionFilters.map(mission => `
-          <div id="missionsContainer" class="row g-4">
-                    <div class="card mission-card h-100 p-3">
-                            <div class="card-body d-flex flex-column">
-                              <h5 class="card-title">${mission.title}</h5>
-                              <h6 class="card-subtitle mb-2 text-muted">${mission.company}</h6>
-                              <p class="card-text mt-2">${mission.description}</p>
-
-                              <div class="mt-auto">
-                                <span class="badge me-2"><i class="bi bi-geo-alt"></i> ${mission.location}</span>
-                                <span class="badge bg-secondary me-2">${mission.duration}</span>
-                                <span class="badge bg-success">${mission.budget}</span>
-                              </div>
-                            </div>
-                    </div>
-            </div>
-        `)
+                if(valeurChoisie == "Par Technologie"){
+                  alert("veuillez selectionr une technologie a rechercher ");
+                  return;
+                }
+              
+                const missionFilters = datamission.filter(mission => mission.title === valeurChoisie)
+                console.log(missionFilters);
+              
+              
+                  sectionMission.innerHTML = missionFilters.map(mission => `
+                      <div id="missionsContainer" class="row g-4">
+                                <div class="card mission-card h-100 p-3">
+                                        <div class="card-body d-flex flex-column">
+                                          <h5 class="card-title">${mission.title}</h5>
+                                          <h6 class="card-subtitle mb-2 text-muted">${mission.company}</h6>
+                                          <p class="card-text mt-2">${mission.description}</p>
+                  
+                                          <div class="mt-auto">
+                                            <span class="badge me-2"><i class="bi bi-geo-alt"></i> ${mission.location}</span>
+                                            <span class="badge bg-secondary me-2">${mission.duration}</span>
+                                            <span class="badge bg-success">${mission.budget}</span>
+                                          </div>
+                                        </div>
+                                </div>
+                        </div>
+                    `)
   })
+
   
 
   }
-
-
-
-
-
 filtrageMission()
 
 
